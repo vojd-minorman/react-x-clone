5d30f6c2b24a97c18803b6ded3a7bfc9c95a1bcc
Neville Mitshiabu <mitshneville7@gmail.com>
2024-10-03 12:23:28 +0100

    suppression de l'element non utilisé React

--- src/components/timeline/tweet/Tweet.tsx
+++ src/components/timeline/tweet/Tweet.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MessageCircle, Repeat2, Heart, Share } from 'lucide-react';
 
